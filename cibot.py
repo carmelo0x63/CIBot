dafbf9bfd7aa26289fddfbbe5ad0b215ea75c009
Carmelo C <carmelo.califano@gmail.com>
2022-06-05 18:20:33 +0200

    Moved config to external file 2

--- cibot.py
+++ cibot.py
@@ -14,16 +14,14 @@ import time          # Time access and conversions
 # General settings
 # source: https://core.telegram.org/api
 BASEURL = 'https://api.telegram.org/'
-BOT = <BOTNAME>
-TOKEN = <TOKEN>
-CHAT_ID = <CHAT_ID>
 LOGFILE = '/var/log/auth.log'
 
 # Version info
 __version__ = "1.0"
 __build__ = "20220605"
 
-def read_conf():
+
+def readConf():
     """ 
     read_conf() reads the application's configuration from an external file.
     The file is JSON-formatted and contains:
@@ -31,16 +29,20 @@ def read_conf():
       the token,
       the chat ID.
     """
-    with open(name + '_config.json', 'r') as config_in:
+    with open('cibot.json', 'r') as config_in:
         config_json = json.load(config_in)
     return config_json
 
-def tgsend(message):
+
+def tgSend(message, token, chatid):
     """
-    tgsend() consumes Telegram API sendMessage method to publish a message on the chat
+    tgSend() consumes Telegram API sendMessage method to publish a message on the chat.
+    Required arguments are:
+      the token,
+      the chat ID.
     """
     headers = {'Accept': 'application/json'}
-    url = BASEURL + 'bot' + TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&text=' + message
+    url = BASEURL + 'bot' + token + '/sendMessage?chat_id=' + chatid + '&text=' + message
     response = requests.post(url = url, headers = headers)
 
 
@@ -49,7 +51,7 @@ def main():
     LASTHOUR = time.asctime()[4:13]  # E.g.: 'Jun  5 11'
 #    LASTHOUR = 'Jun  5 11'
     BADGUY = 'Invalid'
-    config = read_conf()
+    config = readConf()
 
     try:
         # Parses LOGFILE within LASTHOUR
@@ -64,7 +66,10 @@ def main():
         msg1 = 'No'
 
     msg2 = ' access attempts in the last hour'
-    tgsend(msg1 + msg2)
+#    bot = config['BOT']
+    token = config['TOKEN']
+    chatid = config['CHATID']
+    tgSend(msg1 + msg2, token, chatid)
 
 
 # Main function
