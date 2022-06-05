0b711b8a7e1fb46cc7cd49c60f20f5a33afd117b
Carmelo C <carmelo.califano@gmail.com>
2022-06-05 18:11:06 +0200

    Moved config to external file

--- cibot.py
+++ cibot.py
@@ -6,6 +6,7 @@
 #  2022-06-05 1.0 Initial version
 
 # Import some modules
+import json          # JSON encoder and decoder
 import requests      # Python HTTP for Humans
 import subprocess    # Subprocess management
 import time          # Time access and conversions
@@ -22,8 +23,22 @@ LOGFILE = '/var/log/auth.log'
 __version__ = "1.0"
 __build__ = "20220605"
 
+def read_conf():
+    """ 
+    read_conf() reads the application's configuration from an external file.
+    The file is JSON-formatted and contains:
+      the bot name,
+      the token,
+      the chat ID.
+    """
+    with open(name + '_config.json', 'r') as config_in:
+        config_json = json.load(config_in)
+    return config_json
 
 def tgsend(message):
+    """
+    tgsend() consumes Telegram API sendMessage method to publish a message on the chat
+    """
     headers = {'Accept': 'application/json'}
     url = BASEURL + 'bot' + TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&text=' + message
     response = requests.post(url = url, headers = headers)
@@ -34,6 +49,7 @@ def main():
     LASTHOUR = time.asctime()[4:13]  # E.g.: 'Jun  5 11'
 #    LASTHOUR = 'Jun  5 11'
     BADGUY = 'Invalid'
+    config = read_conf()
 
     try:
         # Parses LOGFILE within LASTHOUR
