66470f84c34047e5a13444f64fd32a95fa70ba11
Carmelo C <carmelo.califano@gmail.com>
2022-06-06 17:59:29 +0200

    Replaced plain print statements with logging module

--- cibot.py
+++ cibot.py
@@ -3,13 +3,17 @@
 # author: Carmelo C
 # email: carmelo.califano@gmail.com
 # history, date format ISO 8601:
+#  2022-06-06 1.1 Replaced plain print statements with logging module
 #  2022-06-05 1.0 Initial version
 
 # Import some modules
-import json          # JSON encoder and decoder
-import requests      # Python HTTP for Humans
-import subprocess    # Subprocess management
-import time          # Time access and conversions
+import json                                   # JSON encoder and decoder
+import logging                                # Logging facility for Python
+import logging.handlers
+from logging.handlers import SysLogHandler
+import requests                               # Python HTTP for Humans
+import subprocess                             # Subprocess management
+import time                                   # Time access and conversions
 
 # General settings
 # source: https://core.telegram.org/api
@@ -17,8 +21,8 @@ BASEURL = 'https://api.telegram.org/'
 LOGFILE = '/var/log/auth.log'
 
 # Version info
-__version__ = "1.0"
-__build__ = "20220605"
+__version__ = "1.1"
+__build__ = "20220606"
 
 
 def readConf():
@@ -48,6 +52,11 @@ def tgSend(message, token, chatid):
 
 def main():
     # Initialization
+    logger = logging.getLogger('cilogger')
+    logger.setLevel(logging.INFO)
+    handler = logging.handlers.SysLogHandler(facility = SysLogHandler.LOG_DAEMON, address = '/dev/log')
+    logger.addHandler(handler)
+
     LASTHOUR = time.asctime()[4:13]  # E.g.: 'Jun  5 11'
 #    LASTHOUR = 'Jun  5 11'
     BADGUY = 'Invalid'
@@ -59,10 +68,10 @@ def main():
         # Searches the above output for BADGUY
         grep2 = subprocess.check_output(['grep', BADGUY], stdin = grep1.stdout)
         count = len(grep2.splitlines())
-        print(f'[!] {count} unauthorized access attempts in the last hour')
+        logger.info(f'[!] {count} unauthorized access attempts in the last hour')
         msg1 = str(count) + ' unauthorized'
     except:
-        print('[+] No access attempts in the last hour')
+        logger.info('[+] No access attempts in the last hour')
         msg1 = 'No'
 
     msg2 = ' access attempts in the last hour'
